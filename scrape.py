5c6423d3e5ac9a52248873412d5c941066581d65
huysuy05 <huynguyen23@augustana.edu>
2025-04-07 21:36:01 -0500

    Fixing the excel format + Adding values in a row

--- scrape.py
+++ scrape.py
@@ -25,35 +25,24 @@ CELL_SELECTOR = "div[role='gridcell'].ag-cell"
 # Next button elements so we can simulate clicking with selenium
 NEXT_BUTTON = "//div[@class='ag-button ag-paging-button' and contains(@aria-label, 'Next Page')]"
 
+# Horizontal scroll for AG Grid
+HORIZONTAL_SCROLL = "div.ag-body-horizontal-scroll-viewport"
+
 
 # Since the actual data are huge (More than 8 million rows), 
 # We set a limit to how much data selenium can crawl
-NUM_TIMES_TO_EXECUTE = 100
+NUM_TIMES_TO_EXECUTE = 2
 
 OUPUT_DIR = "datasets/crawled_crime_rate_chicago.csv"
 # --- COLUMN NAMES TO BE WRITTEN IN THE CSV FILE --- 
-COL_NAMES = ["id", 
-             "CaseNumber", 
-             "Date", 
-             "Block", 
-             "ICUR", 
-             "PrimaryType", 
-             "Description", 
-             "LocationDescription",
-             "Arrest",
-             "Domestic",
-             "Beat",
-             "District",
-             "Ward",
-             "CommunityArea",
-             "FBICode",
-             "XCoordinate",
-             "YCoordinate",
-             "Year",
-             "UpdatedOn",
-             "Latitude",
-             "Longitude",
-             "Location"]
+COL_NAMES = [
+    "ID", "CaseNo", "Date", "Block", "IUCR", "PrimaryType", "SecondaryType", 
+    "LocDesc", "Arrest", "Domestic", "Beat", "District", "Ward", "CommunityArea", 
+    "FBI", "XCoord", "YCoord", "Year", "UpdatedOn", "Lat", "Long", "Location", 
+    "HistWard", "Zip", "CommArea", "CensusTract", "WardName", "ZipBound", 
+    "PoliceDistrict", "PoliceBeat"
+]
+
 
 print("--- STARTING CRAWLING PROCESS ---")
 
@@ -61,8 +50,9 @@ print("--- STARTING CRAWLING PROCESS ---")
 # --- WEBDRIVER SETUP ---
 service = Service(ChromeDriverManager().install()) #Chrome Drive manager to be able to run chrome
 driver = webdriver.Chrome(service=service)
-print(f"Navigating to {TARGET_URL}...")
+print("Navigating to the website...")
 driver.get(TARGET_URL)
+
 
 # --- SCRAPING LOOP ---
 all_data = [] #Array to be saved later
@@ -70,25 +60,40 @@ try:
     print(f"--- CRAWLING, LOCKING INNNN ---")
     for _ in range(NUM_TIMES_TO_EXECUTE):
         try:
-            WebDriverWait(driver, 10).until(
+            WebDriverWait(driver, 60).until(
                 EC.presence_of_element_located(GRID_ROOT_SELECTOR) # Find the exact element of the table since it's an AG Grid
             )
-            
-            # Find cell data from with a row, for every row that is visible on the browser
-            # Use a method from selenium to query specific elements using Java Script
-            rows_data = driver.execute_script("""
-                return Array.from(document.querySelectorAll('div[role="row"]')).map(row => {
-                    return Array.from(row.querySelectorAll('div[role="gridcell"]')).map(cell => cell.innerText);
-                });
-            """)
 
-            # Process the rows_data
-            for row_data in rows_data:
-                # Ensure row_data matches the length of COL_NAMES
-                while len(row_data) < len(COL_NAMES):
-                    row_data.append("")  # Add empty strings for missing columns
-                print(f"Row Data: {row_data}")
-                all_data.append(row_data)
+            # Scroll horizontally
+            scroll_container = driver.find_element(By.CSS_SELECTOR, HORIZONTAL_SCROLL)
+            last_scroll_pos = 0
+
+            # Current position
+            while True:
+                # All data in a row after scrolling
+                all_row_data = []
+                # Find cell data from with a row, for every row that is visible on the browser
+                # Use a method from selenium to query specific elements using Java Script
+                rows_data = driver.execute_script("""
+                    return Array.from(document.querySelectorAll('div.ag-center-cols-container div[role="row"]')).map(row => {
+                        const cells = row.querySelectorAll('div[role="gridcell"]');
+                        return Array.from(cells).map(cell => cell.innerText.trim());
+                    });
+                """)
+                
+                all_data.append([row for row in rows_data])
+
+                # Scroll to the right
+                driver.execute_script("arguments[0].scrollLeft += 1000", scroll_container)
+                time.sleep(3)
+
+                curr_scroll_pos = driver.execute_script("return arguments[0].scrollLeft", scroll_container)
+                if curr_scroll_pos == last_scroll_pos:
+                    break
+                last_scroll_pos = curr_scroll_pos
+
+
+            all_data.append(all_row_data)
 
             try:
                 # Locate the next button
@@ -100,8 +105,9 @@ try:
                     print("Could not locate the next button. Quitting the program...")
                     break
 
-                # Simulate clicking the next button
+                # Simulate clicking the next button and reset the screen to the default location
                 next_btn.click()
+                driver.execute_script("arguments[0].scrollLeft = 0", scroll_container)
                 # Wait for the JS to load all necessary file
                 time.sleep(3)
             except TimeoutException:
@@ -129,7 +135,8 @@ try:
         print("No data was saved during the crawling process.")
     else:
         try:
-            df = pd.DataFrame(all_data, columns=COL_NAMES)
+            
+            df = pd.DataFrame(all_data)
             df.to_csv(OUPUT_DIR, index=False)
             print(f"Data saved successfully to {OUPUT_DIR}")
         except Exception as e:
