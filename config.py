e24d708bf26c1518f5bb1cf99ad9f6684567cb54
huysuy05 <huynguyen23@augustana.edu>
2025-05-10 02:43:31 -0500

    Trained and Evaluated different classifcation models

---
+++ config.py
@@ -0,0 +1,8 @@
+# folder files
+TRAINING_FILE = "datasets/cleaned_crime_data.csv"
+CRAWLED_DATA = "datasets/crawled_data.csv"
+LOGISTIC_REGRESSION_MODELS = "models/Logistic_Regression/"
+RANDOM_FOREST_MODELS = "models/Random_Forest/"
+KNN_MODELS = "models/KNN/"
+XGBOOST_MODELS = "models/XGBoost/"
+DECISION_TREE_MODELS = "models/Decision_Tree/" 
